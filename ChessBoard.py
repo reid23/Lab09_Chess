59624eb94eac4c9b945affe144e59682482f363f
Reid Dye <reid.dye@gmail.com>
2022-01-26 22:18:41 -0800

    Board class is now iterable!  documentation needs some work though

--- ChessBoard.py
+++ ChessBoard.py
@@ -1,5 +1,5 @@
 from graphics import Rectangle, Point
-import numpy as np #only needed for 
+import numpy as np #only needed for test func
 
 class Board:
     def __init__(self, *pieces):
@@ -8,10 +8,13 @@ class Board:
         Args:
             *pieces (piece objects): the chess pieces for this board.
         """
-        self.pieces=pieces
+        self.n=0
+        self.iterReturns='squares'
+        self.pieces=set(pieces)
         self.gameState = self._empty(8,8,3)
+        self.descriminator=lambda x: True
         for p in pieces:
-            self.putThing(p, p.getStartPos()) #add this to pieces
+            self.putThing(p, p.startPos) #add this to pieces
         for x, y in [[x, y] for x in range(8) for y in range(8)]:
             self.putThing(Rectangle(Point(x-0.5, y-0.5), Point(x+0.5, y+0.5)), (x, y), thingType='tile')
             self.putThing(bool(x%2 ^ y%2), (x, y), thingType='lit') #white true, black false
@@ -70,6 +73,20 @@ class Board:
         else:
             return t
 
+
+    def T(self, arr=None):
+        if arr==None: arr=self.gameState
+
+        output=arr.copy()
+
+        for i in range(len(arr)):
+            for j in range(len(i)):
+                output[i][j]=arr[j][i]
+
+        return output
+
+
+
     def putThing(self, thing, position, thingType='piece'):
         """puts $thing into the board at $position
 
@@ -85,6 +102,8 @@ class Board:
                 self.gameState[position[1]][position[0]][1]=thing
             case 'piece':
                 self.gameState[position[1]][position[0]][2]=thing
+                self.pieces.add(thing)
+
 
     def unPutThing(self, thing, actuallyRemove=True, replacement=None):
         """finds and removes $thing from the board, and replaces it with $replacement.  Only actually does this if $actuallyremove
@@ -104,12 +123,112 @@ class Board:
                     try:
                         if isinstance(k, thing):
                             poses.append([x,y,z])
-                            if actuallyRemove: self.gameState[y][x][z]=replacement
+                            if actuallyRemove: 
+                                self.gameState[y][x][z]=replacement
+                                if k in self.pieces: self.pieces.remove(k)
                     except TypeError:
                         if k==thing:
                             poses.append([x,y,z])
-                            if actuallyRemove: self.gameState[y][x][z]=replacement
+                            if actuallyRemove: 
+                                self.gameState[y][x][z]=replacement
+                                if k in self.pieces: self.pieces.remove(k)
         return poses
+    
+    def __call__(self, things='all', descriminator=None):
+        """call method for board, for generating an iterable for for loops.  ie 'for _ in Board(): pass'
+
+        Args:
+            things (str, optional): the things to iterate through: 'all', 'lit', 'title', 'piecesInBoard', 'pieces', 'rows', or 'columns'. Defaults to 'all', giving a len-3 list.
+            descriminator (str|function, optional): returns True only if output should be kept, like "lambda x: x.getColor()=='Black'" or whatever to only iterate through the black pieces. Screens outputs. Defaults to None.
+
+        Returns:
+            Board: this board object, passed to for loop with correct instance vars for appropriate iteration
+        """
+
+
+        if descriminator==None: self.descriminator=lambda x: True
+        else: self.descriminator=descriminator
+
+        self.iterReturns=things
+        return self
+
+    def __iter__(self):
+        self.n=0
+        return self
+
+    def __next__(self):
+        if self.iterReturns=='all':
+
+            if self.n==65: raise StopIteration
+
+            while True:
+                output = self.gameState[int(self.n/8.05)][self.n%8]
+                if self.descriminator(output): break
+                self.n+=1
+
+        elif self.iterReturns=='tile':
+
+            if self.n==65: raise StopIteration
+
+            while True:
+                output = self.gameState[int(self.n/8.05)][self.n%8][0]
+                if self.descriminator(output): break
+                self.n+=1
+        
+        elif self.iterReturns=='lit':
+
+            if self.n==65: raise StopIteration
+
+            while True:
+                output = self.gameState[int(self.n/8.05)][self.n%8][1]
+                if self.descriminator(output): break
+                self.n+=1
+
+        elif self.iterReturns=='piecesInBoard':
+
+            if self.n==65: raise StopIteration
+
+            while True:
+                output = self.gameState[int(self.n/8.05)][self.n%8][2]
+                if self.descriminator(output): break
+                self.n+=1
+
+        elif self.iterReturns=='pieces':
+
+            if self.n==len(self.pieces): raise StopIteration
+
+            while True:
+                output = list(self.pieces)[self.n]
+                if self.descriminator(output): break
+                self.n+=1
+
+
+        elif self.iterReturns=='rows':
+
+            if self.n==8: raise StopIteration
+
+            while True:
+                output=self.gameState[self.n]
+                if self.descriminator(output): break
+                self.n+=1
+        
+
+        elif self.iterReturns=='columns':
+
+            if self.n==8: raise StopIteration
+
+            while True:
+                output=self.T()[self.n]
+                if self.descriminator(output): break
+                self.n+=1
+
+        self.n+=1
+        return output
+
+
+
+
+
 
 
 
@@ -129,3 +248,7 @@ if __name__ == '__main__':
     B.unPutThing('test')
 
     print(B.unPutThing(str, False))
+
+    for thing in B('piecesInBoard'):
+        print(thing)
+
