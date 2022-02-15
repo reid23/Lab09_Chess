61ffd7bf0e71534dc873b904ca16491cf569721f
Reid Dye <reid.dye@gmail.com>
2022-02-15 10:58:26 -0800

    oops forgot a file

--- noPiece.py
+++ noPiece.py
@@ -1,7 +1,6 @@
 class noPiece(ChessPiece):
     def __init__(self, *args, **kwargs):
         super().__init__(None, (420, 420))
-        self._color = None
 
     def calculatePossibleMoves(self, gameState: list, pos: tuple) -> list:
         return []
