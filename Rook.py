c8602dd264f3ce5d6ee6428826c9f4dc85d5c618
Reid Dye <reid.dye@gmail.com>
2022-02-15 11:00:40 -0800

    roooooooook woooooorks maybe

--- Rook.py
+++ Rook.py
@@ -49,12 +49,25 @@ class Rook(ChessPiece):
             Returns:
                 list of moves (filters out of bounds)
             """
-            moves = []
-            for l in self.rules:
-                for rel in l:
-                    move = (pos[0]+rel[0], pos[1]+rel[1]) 
-                    # if move is within bounds
-                    if self.withinBounds(move):
-                        moves.append(move)
+            nxs, nys, pxs, pys = (list(i) for i in self.rules)
 
-            return moves
+            #need to check:
+            #   is spot occupied by own color piece
+            #   is spot blocked by any piece
+            #   self.checkCheck
+            #   within bounds
+
+            for counter, mov in enumerate(nxs):
+                if not self.withinBounds(self._toGlobal(pos, mov)): #if the move is outside the board...
+                    del nxs[counter+1:]
+                    break
+                if isinstance(gameState[mov[0]][mov[1]][2], ChessPiece):
+                    del nxs[counter+1:] #remove all moves blocked by this piece
+                    if gameState[mov[0]][mov[1]][2].color==self.color:
+                        del nxs[counter] #delete this space too if it's the same color as me or if it would cause a checkmate
+                    break #then break, no more deciding to do for this section
+                if self.checkCheck(gameState, pos, mov): #even if it's not a piece, we shouldn't allow check/checkmate
+                    del nxs[counter]
+                    continue
+                
+            return list(set(tuple(nxs)+tuple(nys)+tuple(pxs)+tuple(pys)))
